57c8d5cfbaec4928219d9556acc521a0b9a99ff6
PrashantRaj18198 <prashant@argonaut.dev>
2023-12-16 17:03:23 +0530

    extraction path wrong

--- images/macos/scripts/warpbuild/install-github-runner.sh
+++ images/macos/scripts/warpbuild/install-github-runner.sh
@@ -10,18 +10,18 @@ echo "Creating warpbuild directories ..."
 mkdir -p ~/.warpbuild/github-runner
 BASE_DIR=~/.warpbuild/github-runner
 
-export TARGETARCH=${TARGETARCH:-arm64}
-export TARGETOS=${TARGETOS:-osx}
-export RUNNER_VERSION=${RUNNER_VERSION:-2.311.0}
-export RUNNER_CONTAINER_HOOKS_VERSION=${RUNNER_CONTAINER_HOOKS_VERSION:-0.3.2}
-export RUNNER_ARCH=$TARGETARCH \
-	&& if [ "$RUNNER_ARCH" = "amd64" ]; then export RUNNER_ARCH=x64 ; fi \
-	&& curl -f -L -o $BASE_DIR/runner.tar.gz https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-${TARGETOS}-${RUNNER_ARCH}-${RUNNER_VERSION}.tar.gz \
-	&& tar xzf $BASE_DIR/runner.tar.gz \
-	&& rm $BASE_DIR/runner.tar.gz
+TARGETARCH=arm64
+TARGETOS=osx
+RUNNER_VERSION='2.311.0'
+RUNNER_CONTAINER_HOOKS_VERSION='0.3.2'
+RUNNER_ARCH=$TARGETARCH 
 
-curl -f -L -o $BASE_DIR/runner-container-hooks.zip https://github.com/actions/runner-container-hooks/releases/download/v${RUNNER_CONTAINER_HOOKS_VERSION}/actions-runner-hooks-k8s-${RUNNER_CONTAINER_HOOKS_VERSION}.zip \
-	&& unzip $BASE_DIR/runner-container-hooks.zip -d ./k8s \
-	&& rm $BASE_DIR/runner-container-hooks.zip
+curl -f -L -o $BASE_DIR/runner.tar.gz https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-${TARGETOS}-${RUNNER_ARCH}-${RUNNER_VERSION}.tar.gz 
+tar xzf $BASE_DIR/runner.tar.gz -C $BASE_DIR
+rm $BASE_DIR/runner.tar.gz
+
+curl -f -L -o $BASE_DIR/runner-container-hooks.zip https://github.com/actions/runner-container-hooks/releases/download/v${RUNNER_CONTAINER_HOOKS_VERSION}/actions-runner-hooks-k8s-${RUNNER_CONTAINER_HOOKS_VERSION}.zip
+unzip $BASE_DIR/runner-container-hooks.zip -d $BASE_DIR/k8s
+rm $BASE_DIR/runner-container-hooks.zip
 
 echo "Runner download complete ..."
