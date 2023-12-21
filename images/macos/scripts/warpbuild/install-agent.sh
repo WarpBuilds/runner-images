78d66f2b8ad1d868c7c2898a1c42a3d9318e1aa9
PrashantRaj18198 <prashant@argonaut.dev>
2023-12-21 14:03:44 +0530

    feat: start using launcher

--- images/macos/scripts/warpbuild/install-agent.sh
+++ images/macos/scripts/warpbuild/install-agent.sh
@@ -24,42 +24,56 @@ sudo chmod +x /usr/local/bin/warpbuild-agentd
 
 echo "Configuring agent ..."
 
-cat << EOF > $BASE_DIR/com.warpbuild.warpbuild-agentd.plist
+cat << EOF > $BASE_DIR/warpbuild-agentd-launcher.sh
+#!/bin/bash
+
+. $HOME/.bashrc
+
+/usr/local/bin/warpbuild-agentd --settings=$BASE_DIR/settings.json
+EOF
+
+cat << EOF > $BASE_DIR/com.warpbuild.warpbuild-agentd-launcher.plist
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
-<dict>
+  <dict>
     <key>Label</key>
-    <string>com.warpbuild.warpbuild-agentd</string>
+    <string>warpbuild-agentd-launcher</string>
+    <key>Nice</key>
+    <integer>-10</integer>
     <key>ProgramArguments</key>
     <array>
-        <string>/usr/local/bin/warpbuild-agentd</string>
-        <string>--settings=$BASE_DIR/settings.json</string>
+      <string>/usr/local/warpbuild-agentd-launcher.sh</string>
     </array>
+    <key>EnvironmentVariables</key>
+    <dict>
+      <key>LANG</key>
+      <string>en_US.UTF-8</string>
+    </dict>
     <key>UserName</key>
     <string>$USERNAME</string>
-    <key>WorkingDirectory</key>
-    <string>/</string>
     <key>RunAtLoad</key>
     <true/>
-    <key>KeepAlive</key>
-    <true/>
-    <key>StandardErrorPath</key>
-    <string>$BASE_DIR/log/warpbuild-agentd.error.log</string>
-    <key>StandardOutPath</key>
-    <string>$BASE_DIR/log/warpbuild-agentd.out.log</string>
     <key>ThrottleInterval</key>
-    <integer>5</integer>
-</dict>
-</plist>
-EOF
+    <integer>15</integer>
+    <key>KeepAlive</key>
+    <dict>
+      <key>SuccessfulExit</key>
+      <false/>
+    </dict>
+    <key>StandardErrorPath</key>
+    <string>$BASE_DIR/log/warpbuild-agentd-launcher.error.log</string>
+    <key>StandardOutPath</key>
+    <string>$BASE_DIR/log/warpbuild-agentd-launcher.out.log</string>
+  </dict>
+</plist
 
 echo "Adding agent to launchd ..."
-sudo mv $BASE_DIR/com.warpbuild.warpbuild-agentd.plist /Library/LaunchDaemons/com.warpbuild.warpbuild-agentd.plist
+sudo mv $BASE_DIR/com.warpbuild.warpbuild-agentd-launcher.plist /Library/LaunchDaemons/com.warpbuild.warpbuild-agentd-launcher.plist
 echo "Setting agent permissions ..."
-sudo chown root:wheel /Library/LaunchDaemons/com.warpbuild.warpbuild-agentd.plist
+sudo chown root:wheel /Library/LaunchDaemons/com.warpbuild.warpbuild-agentd-launcher.plist
 echo "Loading agent ..."
-sudo launchctl load /Library/LaunchDaemons/com.warpbuild.warpbuild-agentd.plist
+sudo launchctl load /Library/LaunchDaemons/com.warpbuild.warpbuild-agentd-launcher.plist
 echo "Starting agent ..."
 sudo launchctl start com.warpbuild.warpbuild-agentd
 
