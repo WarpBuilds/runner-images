d0eff9a9c8a670606c616a2aafddff03ada57cb5
PrashantRaj18198 <prashant@argonaut.dev>
2023-12-22 11:42:46 +0530

    fix: create runner user dir and enable remote login

--- images/macos/scripts/prepare/setup-runner-user.sh
+++ images/macos/scripts/prepare/setup-runner-user.sh
@@ -9,6 +9,9 @@ echo "Getting current group"
 curr_group=$(id -gn)
 echo "Current group: $curr_group"
 
+echo "Getting current user"
+curr_user=$(id -un)
+echo "Current user: $curr_user"
 
 echo "Creating new runner user through sysadminctl with admin privileges"
 sudo sysadminctl \
@@ -19,3 +22,9 @@ sudo sysadminctl \
   -admin
 
 sudo dscl . -create /Users/$RUNNER_USER PrimaryGroupID "$curr_group"
+
+sudo cp -R /Users/$curr_user/ /Users/$RUNNER_USER/
+sudo chown -R $RUNNER_USER /Users/$RUNNER_USER
+
+# Enable SSH (Remote Login)
+sudo systemsetup -setremotelogin on
