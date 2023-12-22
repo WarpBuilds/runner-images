c471eefd7cbce6e595a5533626da925a6eb3f6c1
PrashantRaj18198 <prashant@argonaut.dev>
2023-12-22 11:49:11 +0530

    debug logs

--- images/macos/scripts/prepare/setup-runner-user.sh
+++ images/macos/scripts/prepare/setup-runner-user.sh
@@ -21,9 +21,15 @@ sudo sysadminctl \
   -home /Users/$RUNNER_USER \
   -admin
 
+echo "Adding $RUNNER_USER to $curr_group"
 sudo dscl . -create /Users/$RUNNER_USER PrimaryGroupID "$curr_group"
 
+echo "Adding $RUNNER_USER to sudoers"
+sudo dscl . -append /Groups/admin GroupMembership $RUNNER_USER
+
+echo "Copying $curr_user home directory to $RUNNER_USER"
 sudo cp -R /Users/$curr_user/ /Users/$RUNNER_USER/
+echo "Changing ownership of $RUNNER_USER home directory to $RUNNER_USER"
 sudo chown -R $RUNNER_USER /Users/$RUNNER_USER
 
 # Enable SSH (Remote Login)
