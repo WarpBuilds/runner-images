8f2b18ecc38bbf593f3fa4f607fe7d9bccf3249d
PrashantRaj18198 <prashant@argonaut.dev>
2024-01-11 13:14:23 +0530

    validation

--- scripts/macos/macOS-13.arm64.tart.push.sh
+++ scripts/macos/macOS-13.arm64.tart.push.sh
@@ -24,6 +24,11 @@ if [ -z "${PREPROD_AWS_REGION}" ]; then
   echo "PREPROD_AWS_REGION is not set"
   exit 1
 fi
+# check if PREPROD_IMAGE_URI is set
+if [ -z "${PREPROD_IMAGE_URI}" ]; then
+  echo "PREPROD_IMAGE_URI is not set"
+  exit 1
+fi
 # check if PROD_IMAGE_HOST is set
 if [ -z "${PROD_IMAGE_HOST}" ]; then
   echo "PROD_IMAGE_HOST is not set"
@@ -44,6 +49,11 @@ if [ -z "${PROD_AWS_REGION}" ]; then
   echo "PROD_AWS_REGION is not set"
   exit 1
 fi
+# check if PROD_IMAGE_URI is set
+if [ -z "${PROD_IMAGE_URI}" ]; then
+  echo "PROD_IMAGE_URI is not set"
+  exit 1
+fi
 
 echo "Logging into ECR for preprod with host $PREPROD_IMAGE_HOST"
 login_pass=$(AWS_ACCESS_KEY_ID=${PREPROD_AWS_ACCESS_KEY_ID} \
@@ -54,8 +64,8 @@ login_pass=$(AWS_ACCESS_KEY_ID=${PREPROD_AWS_ACCESS_KEY_ID} \
 echo $login_pass | \
   tart login --username AWS --password-stdin $PREPROD_IMAGE_HOST
 
-echo "Pushing image to preprod to $PREPROD_IMAGE_URL"
-tart push s2 $PREPROD_IMAGE_URL
+echo "Pushing image to preprod to $PREPROD_IMAGE_URI"
+tart push s2 $PREPROD_IMAGE_URI
 echo "Pushed image to preprod"
 
 echo "Logging into ECR for prod with host $PROD_IMAGE_HOST"
@@ -67,6 +77,6 @@ login_pass=$(AWS_ACCESS_KEY_ID=${PROD_AWS_ACCESS_KEY_ID} \
 echo $login_pass | \
   tart login --username AWS --password-stdin $PROD_IMAGE_HOST
 
-echo "Pushing image to prod to $PROD_IMAGE_URL"
-tart push s2 $PROD_IMAGE_URL
+echo "Pushing image to prod to $PROD_IMAGE_URI"
+tart push s2 $PROD_IMAGE_URI
 echo "Pushed image to prod"
