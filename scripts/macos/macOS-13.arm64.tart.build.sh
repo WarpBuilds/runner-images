083d99e1a88dc5f0c1a0f7f294f786a9846ab517
PrashantRaj18198 <prashant@argonaut.dev>
2024-01-11 13:19:04 +0530

    validation for build

--- scripts/macos/macOS-13.arm64.tart.build.sh
+++ scripts/macos/macOS-13.arm64.tart.build.sh
@@ -3,6 +3,17 @@ set -e
 
 cd images/macos
 
+# check if GITHUB_API_PAT is set
+if [ -z "${GITHUB_API_PAT}" ]; then
+  echo "GITHUB_API_PAT is not set"
+  exit 1
+fi
+# check if XCODE_INSTALL_STORAGE_URL is set
+if [ -z "${XCODE_INSTALL_STORAGE_URL}" ]; then
+  echo "XCODE_INSTALL_STORAGE_URL is not set"
+  exit 1
+fi
+
 echo "Booting up new vm from ipsw base image"
 packer build \
   -var-file ./variables/macos-13.arm64.tart.pkrvars.hcl \
