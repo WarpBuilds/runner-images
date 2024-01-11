f1e62e9b9f63ed3e77acd1409a73dfcfe9c65f8e
PrashantRaj18198 <prashant@argonaut.dev>
2024-01-11 13:07:56 +0530

    validations

--- scripts/macos/macOS-13.arm64.tart.push.sh
+++ scripts/macos/macOS-13.arm64.tart.push.sh
@@ -4,6 +4,47 @@ set -e
 
 cd images/macos
 
+# check if PREPROD_IMAGE_HOST is set
+if [ -z "${PREPROD_IMAGE_HOST}" ]; then
+  echo "PREPROD_IMAGE_HOST is not set"
+  exit 1
+fi
+# check if PREPROD_AWS_ACCESS_KEY_ID is set
+if [ -z "${PREPROD_AWS_ACCESS_KEY_ID}" ]; then
+  echo "PREPROD_AWS_ACCESS_KEY_ID is not set"
+  exit 1
+fi
+# check if PREPROD_AWS_SECRET_ACCESS_KEY is set
+if [ -z "${PREPROD_AWS_SECRET_ACCESS_KEY}" ]; then
+  echo "PREPROD_AWS_SECRET_ACCESS_KEY is not set"
+  exit 1
+fi
+# check if PREPROD_AWS_REGION is set
+if [ -z "${PREPROD_AWS_REGION}" ]; then
+  echo "PREPROD_AWS_REGION is not set"
+  exit 1
+fi
+# check if PROD_IMAGE_HOST is set
+if [ -z "${PROD_IMAGE_HOST}" ]; then
+  echo "PROD_IMAGE_HOST is not set"
+  exit 1
+fi
+# check if PROD_AWS_ACCESS_KEY_ID is set
+if [ -z "${PROD_AWS_ACCESS_KEY_ID}" ]; then
+  echo "PROD_AWS_ACCESS_KEY_ID is not set"
+  exit 1
+fi
+# check if PROD_AWS_SECRET_ACCESS_KEY is set
+if [ -z "${PROD_AWS_SECRET_ACCESS_KEY}" ]; then
+  echo "PROD_AWS_SECRET_ACCESS_KEY is not set"
+  exit 1
+fi
+# check if PROD_AWS_REGION is set
+if [ -z "${PROD_AWS_REGION}" ]; then
+  echo "PROD_AWS_REGION is not set"
+  exit 1
+fi
+
 echo "Logging into ECR for preprod"
 AWS_ACCESS_KEY_ID=${PREPROD_AWS_ACCESS_KEY_ID} \
   AWS_SECRET_ACCESS_KEY=${PREPROD_AWS_SECRET_ACCESS_KEY} \
