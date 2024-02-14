1ea93c35ce393b7441be2dee4e24becca537e388
Abhijit Hota <abhihota025@gmail.com>
2024-02-14 13:08:47 +0530

    remove dependency from github_pat

--- scripts/macos/macOS-13.arm64.tart.build.sh
+++ scripts/macos/macOS-13.arm64.tart.build.sh
@@ -3,11 +3,6 @@ set -e
 
 cd images/macos
 
-# check if GITHUB_API_PAT is set
-if [ -z "${GITHUB_API_PAT}" ]; then
-  echo "GITHUB_API_PAT is not set"
-  exit 1
-fi
 # check if XCODE_INSTALL_STORAGE_URL is set
 if [ -z "${XCODE_INSTALL_STORAGE_URL}" ]; then
   echo "XCODE_INSTALL_STORAGE_URL is not set"
@@ -25,7 +20,6 @@ packer build \
 echo "Setting up the runner-image"
 packer build \
   -var-file ./variables/macos-13.arm64.tart.pkrvars.hcl \
-  -var "github_api_pat=${GITHUB_API_PAT}" \
   -var "xcode_install_storage_url=${XCODE_INSTALL_STORAGE_URL}" \
   templates/macOS-13.arm64.tart.pkr.hcl
 echo "Setting up warpbuild components"
--- scripts/macos/macOS-14.arm64.tart.build.sh
+++ scripts/macos/macOS-14.arm64.tart.build.sh
@@ -3,11 +3,6 @@ set -e
 
 cd images/macos
 
-# check if GITHUB_API_PAT is set
-if [ -z "${GITHUB_API_PAT}" ]; then
-  echo "GITHUB_API_PAT is not set"
-  exit 1
-fi
 # check if XCODE_INSTALL_STORAGE_URL is set
 if [ -z "${XCODE_INSTALL_STORAGE_URL}" ]; then
   echo "XCODE_INSTALL_STORAGE_URL is not set"
@@ -27,7 +22,6 @@ packer build \
 echo "Setting up the runner-image"
 packer build \
   -var-file ./variables/macos-14.arm64.tart.pkrvars.hcl \
-  -var "github_api_pat=${GITHUB_API_PAT}" \
   -var "xcode_install_storage_url=${XCODE_INSTALL_STORAGE_URL}" \
   templates/macOS-14.arm64.tart.pkr.hcl
 
