655823ac6885760d7cd33697a0414d84077c7a00
Prajjwal <prajjwaldimri@hotmail.com>
2024-08-21 17:30:45 +0530

    adds proxyd service

--- images/macos/scripts/warpbuild/install-agent.sh
+++ images/macos/scripts/warpbuild/install-agent.sh
@@ -131,15 +131,70 @@ cat << EOF > $BASE_DIR/com.warpbuild.warpbuild-telemetryd-launcher.plist
 </plist>
 EOF
 
+echo "Adding proxy launcher"
+cat << EOF > $BASE_DIR/warpbuild-proxyd-launcher.sh
+#!/bin/bash
+
+. $HOME/.bashrc
+
+/usr/local/bin/warpbuild-agentd --settings=$BASE_DIR/settings.json --launch-proxy-server
+EOF
+
+echo "Setting launcher permissions ..."
+chmod +x $BASE_DIR/warpbuild-proxyd-launcher.sh
+
+echo "Adding settings ..."
+mv $BASE_DIR/warpbuild-proxyd-launcher.sh /usr/local/bin/warpbuild-proxyd-launcher.sh
+
+cat << EOF > $BASE_DIR/com.warpbuild.warpbuild-proxyd-launcher.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>Label</key>
+    <string>warpbuild-proxyd-launcher</string>
+    <key>Nice</key>
+    <integer>-10</integer>
+    <key>ProgramArguments</key>
+    <array>
+      <string>/usr/local/bin/warpbuild-proxyd-launcher.sh</string>
+    </array>
+    <key>EnvironmentVariables</key>
+    <dict>
+      <key>LANG</key>
+      <string>en_US.UTF-8</string>
+    </dict>
+    <key>UserName</key>
+    <string>$USERNAME</string>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>ThrottleInterval</key>
+    <integer>15</integer>
+    <key>KeepAlive</key>
+    <dict>
+      <key>SuccessfulExit</key>
+      <false/>
+    </dict>
+    <key>StandardErrorPath</key>
+    <string>$BASE_DIR/log/warpbuild-proxyd-launcher.error.log</string>
+    <key>StandardOutPath</key>
+    <string>$BASE_DIR/log/warpbuild-proxyd-launcher.out.log</string>
+  </dict>
+</plist>
+EOF
+
 echo "Adding agent to launchd ..."
 LAUNCH_AGENTS_DIR=~/Library/LaunchAgents
 mkdir -p $LAUNCH_AGENTS_DIR
 mv $BASE_DIR/com.warpbuild.warpbuild-agentd-launcher.plist $LAUNCH_AGENTS_DIR/com.warpbuild.warpbuild-agentd-launcher.plist
 mv $BASE_DIR/com.warpbuild.warpbuild-telemetryd-launcher.plist $LAUNCH_AGENTS_DIR/com.warpbuild.warpbuild-telemetryd-launcher.plist
+mv $BASE_DIR/com.warpbuild.warpbuild-telemetryd-launcher.plist $LAUNCH_AGENTS_DIR/com.warpbuild.warpbuild-proxyd-launcher.plist
 echo "Loading agent ..."
 launchctl load -w $LAUNCH_AGENTS_DIR/com.warpbuild.warpbuild-agentd-launcher.plist || true
 echo "Loading telemetry agent ..."
 launchctl load -w $LAUNCH_AGENTS_DIR/com.warpbuild.warpbuild-telemetryd-launcher.plist || true
+echo "Loading proxy agent ..."
+launchctl load -w $LAUNCH_AGENTS_DIR/com.warpbuild.warpbuild-proxyd-launcher.plist || true
 
 echo "Give permission to runner user to read /var/log/system.log"
 sudo chmod +a "runner allow read" /var/log/system.log
